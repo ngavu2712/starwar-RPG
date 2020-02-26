6612595535387cf3f41c44c44ea304a47376e5a4
Anna Vu <ngavu2712@gmail.com>
2020-02-26 14:21:25 -0700

    I readjust the character structure on html in order to be able to move the block later after applying js

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+//STEP1: click on one character, that chosen character will stay in the same column while the rest move to enemy
+//STEP2: click on one of the enemy character, the chosen move to defender while the rest stay in the same column. 
+//STEP3: click Attack to damage each other health point, these point are shown at the bottom of your character and enemy pictures. 
+//STEP4: If your character HP reach 0 before enemy, you lose. 
+//      If enemy HP reach 0 before you, enemy image will disappear and you choose new defender. You only win until you defeat all three character.
+
+
+//STEP1: Create Health point, Attack power and Counter Attack Power  variables. Make the HP variable random from 100-170 for each character.
+//      Attack power is fixed at 9 , and the next attack will increase by 9. This variable belongs to your character.
+//      Counter attack power is fixed at 17. This variable belong to enemy.
